3e7ab634567e08395014a6588f693f7cebba5253
Libitum <libitum@msn.com>
2012-03-05 13:01:37 +0800

    init project

---
+++ src/catkin/psketcher/PSketcherActivity.java
@@ -0,0 +1,13 @@
+package catkin.psketcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PSketcherActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
